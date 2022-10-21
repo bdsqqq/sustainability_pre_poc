0b916b96ea27fa7ed31df37f355bedfcb81963c5
Igor Bedesqui <Igor.Bedesqui@ibm.com>
2022-10-21 14:55:46 -0300

    feat: navigation + style tweaks

--- src/pages/[pillar].tsx
+++ src/pages/[pillar].tsx
@@ -9,71 +9,92 @@ const Pillar = ({
   pillarData,
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
-    <main>
-      <div className="flex min-h-screen">
-        <div className="relative aspect-square md:min-h-screen">
-          <figure className="h-full w-full">
-            <img
-              className="h-full w-full"
-              src={pillarData}
-              alt="abstract image"
+    <div key={pillarData} className="grid [grid-template-rows:auto_48px]">
+      <Navigation className="row-start-2" />
+      <main className="h-[calc(100vh-48px)] overflow-scroll">
+        <div className="flex min-h-[calc(100vh-48px)]">
+          <div className="relative aspect-square min-h-[calc(100vh-48px)]">
+            <figure className="h-full w-full">
+              <img
+                className="h-full w-full"
+                src={pillarData}
+                alt="abstract image"
+              />
+            </figure>
+            <Hotspot
+              content={
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Praesentium ab voluptates nostrum excepturi odio consequatur
+                  similique, unde sunt architecto ipsa odit ex quia corporis
+                  itaque blanditiis commodi mollitia, aperiam quibusdam.
+                </p>
+              }
+              coords={{ x: 10, y: 10 }}
             />
-          </figure>
-          <Hotspot
-            content={
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Praesentium ab voluptates nostrum excepturi odio consequatur
-                similique, unde sunt architecto ipsa odit ex quia corporis
-                itaque blanditiis commodi mollitia, aperiam quibusdam.
-              </p>
-            }
-            coords={{ x: 10, y: 10 }}
-          />
-          <Hotspot
-            content={
-              <figure>
-                <img
-                  className="h-full w-full"
-                  src={
-                    "https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
-                  }
-                  alt="abstract image"
-                />
-              </figure>
-            }
-            coords={{ x: 30, y: 50 }}
-          />
-          <Hotspot
-            content={
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Praesentium ab voluptates nostrum excepturi odio consequatur
-                similique, unde sunt architecto ipsa odit ex quia corporis
-                itaque blanditiis commodi mollitia, aperiam quibusdam.
-              </p>
-            }
-            coords={{ x: 90, y: 90 }}
-          />
+            <Hotspot
+              content={
+                <figure>
+                  <img
+                    className="h-full w-full"
+                    src={
+                      "https://images.unsplash.com/photo-1618005198919-d3d4b5a92ead?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
+                    }
+                    alt="abstract image"
+                  />
+                </figure>
+              }
+              coords={{ x: 30, y: 50 }}
+            />
+            <Hotspot
+              content={
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Praesentium ab voluptates nostrum excepturi odio consequatur
+                  similique, unde sunt architecto ipsa odit ex quia corporis
+                  itaque blanditiis commodi mollitia, aperiam quibusdam.
+                </p>
+              }
+              coords={{ x: 90, y: 90 }}
+            />
+          </div>
+          <div className="flex w-fit flex-grow-0 flex-col gap-16 p-8">
+            <h1 className="text-2xl">
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              atque at recusandae nulla libero sapiente culpa corrupti provident
+              voluptas eum minima tempore illo dolor impedit quod vel
+              perspiciatis reprehenderit harum accusamus hic nesciunt facere,
+              commodi fuga placeat! Eaque, exercitationem numquam.
+            </p>
+          </div>
         </div>
-        <div className="flex w-fit flex-grow-0 flex-col gap-16 p-8">
-          <h1 className="text-2xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing.
-          </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
-            atque at recusandae nulla libero sapiente culpa corrupti provident
-            voluptas eum minima tempore illo dolor impedit quod vel perspiciatis
-            reprehenderit harum accusamus hic nesciunt facere, commodi fuga
-            placeat! Eaque, exercitationem numquam.
-          </p>
-        </div>
-      </div>
-    </main>
+        <div className="flex min-h-[calc(100vh-48px)]">Other content</div>
+      </main>
+    </div>
   );
 };
 
 export default Pillar;
+
+const Navigation = ({ className }: { className?: string }) => {
+  return (
+    <ul
+      className={`flex h-[48px] items-center gap-16  bg-stone-300 px-8 ${className}`}
+    >
+      <li>
+        <Link href={"/"}>Home</Link>
+      </li>
+      {pillarImages.map((image, i) => (
+        <li key={image}>
+          <Link href={`/${i + 1}`}>{`Pillar ${i + 1}`}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 import {
   Popover,
@@ -83,6 +104,7 @@ import {
   usePopoverState,
 } from "ariakit/popover";
 import { RadioButton, RadioButtonChecked } from "@carbon/icons-react";
+import Link from "next/link";
 const Hotspot = ({
   coords,
   content,
